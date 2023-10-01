1fc6883ca45e95917fba9fb5635c3f85f04f3c03
koskirillal <koskirillal@gmail.com>
2023-10-01 22:05:57 +0300

    dz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
-
+import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
 
-        System.out.println(" ");
-        for (int i = 1; i <= 5; i++) {
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        if (x > 0 && x < 2 && y > 0 && y < 2 && x * x + y * y > 4 && y < x){
+            System.out.println("YES");
+        }else System.out.println("NO");
 
-
-            System.out.printf("i = " + i + " ");
-        }
     }
 }
